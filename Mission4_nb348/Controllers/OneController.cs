b46e715b38bcf27531b34448d5b4150159769545
nbain00 <zink47@gmail.com>
2023-02-06 20:53:41 -0700

    mvc

---
+++ Mission4_nb348/Controllers/OneController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission4_nb348.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_nb348.Controllers
+{
+    public class OneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalc() // Get method
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalc(GradeCalcModel model) // Post method that receives model
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission4_nb348/Models/GradeCalcModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_nb348.Models
+{
+    public class GradeCalcModel
+    {
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")] // Data Validation
+        public int Assignments { get; set; }
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public int Quizzes { get; set; }
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public int Group { get; set; }
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public int Intex { get; set; }
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public int Exam { get; set; }
+    }
+}
